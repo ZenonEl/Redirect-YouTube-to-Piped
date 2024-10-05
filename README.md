87b8cf222b0b4cb8fbb0542adab9764fa3f63e35
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2024-10-05 13:45:02 +0400

    first publication

---
+++ README.md
@@ -0,0 +1,30 @@
+# Переключатель YouTube и Piped
+
+Это проект представляет собой пользовательский скрипт для браузера, который позволяет легко переключаться между YouTube и Piped.
+
+## Установка
+
+Для установки этого скрипта выполните следующие шаги:
+
+1. Установите менеджер пользовательских скриптов. Рекомендую использовать Tampermonkey.
+
+   - Для Chrome: [Tampermonkey для Chrome](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+   - Для Firefox: [Tampermonkey для Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+   - Для Opera: [Tampermonkey для Opera](https://addons.opera.com/en/extensions/details/tampermonkey/)
+   - Для Edge: [Userscripts для Edge](https://microsoftedge.microsoft.com/addons/detail/userscripts-for-edge/gijhjkpggfnjjgkjkggcfjjnjjjjjjjj)
+   - Для Safari (macOS/iOS/iPadOS): [Userscripts для Safari](https://safari-extensions.apple.com/ru/app/userscripts/)
+
+2. После установки Tampermonkey, перейдите по следующей ссылке, чтобы установить этот скрипт:
+   [Установить скрипт](https://raw.githubusercontent.com/ZenonEl/redirect-youtube-to-piped/main/src/index.js)
+
+## Использование
+
+После установки скрипта, он автоматически создаст кнопку на видео, которая позволит вам переключаться между YouTube и Piped или обратно.
+
+## Атрибуция
+
+Иконка проекта была взята с сайта [Flaticon](https://www.flaticon.com/free-icons/redirect). Автор иконки - [redempticon](https://www.flaticon.com/authors/redempticon).
+
+## Лицензия
+
+Этот проект распространяется под лицензией GPL-3.0. Полный текст лицензии доступен в файле [LICENSE](LICENSE).
---
+++ src/Redirect YouTube to Piped-1.1.user.js
@@ -0,0 +1,142 @@
+// ==UserScript==
+// @name            Easy way to quickly switch between YouTube and Piped
+// @version         0.1.0
+// @author          ZenonEl (https://github.com/ZenonEl)
+// @license         GPL v3
+// @match           https://www.youtube.com/*
+// @match           https://piped.video/*
+// @grant           none
+// @run-at          document-end
+// @compatible      chrome
+// @compatible      firefox
+// @compatible      opera
+// @compatible      edge
+// @compatible      safari
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let redirectEnabled = false;
+    let youtubeToggle;
+    let pipedToggle;
+
+    function manageToggles() {
+        if (window.location.hostname === 'www.youtube.com') {
+            if (!youtubeToggle) {
+                positionYoutubeToggleButton();
+            }
+        } else if (window.location.hostname === 'piped.video') {
+            if (!pipedToggle) {
+                positionPipedToggleButton();
+            }
+        } else {
+            if (youtubeToggle) {
+                youtubeToggle.remove();
+                pipedToggle?.remove();
+                youtubeToggle = null;
+                pipedToggle = null;
+            }
+        }
+    }
+
+    function toggleRedirect(event) {
+        event.preventDefault();
+        redirectEnabled = !redirectEnabled;
+
+        if (redirectEnabled) {
+            const targetUrl = window.location.hostname === 'www.youtube.com'
+                ? window.location.href.replace('www.youtube.com', 'piped.video')
+                : window.location.href.replace('piped.video', 'www.youtube.com');
+            window.location.href = targetUrl;
+        }
+
+        const toggleText = redirectEnabled ? 'Перенаправление...' : 'Просмотреть на YouTube';
+        updateToggleButtonText(youtubeToggle, pipedToggle, toggleText);
+    }
+
+    function positionYoutubeToggleButton() {
+        const videoContainer = document.querySelector('#actions');
+        if (!videoContainer) return;
+
+        youtubeToggle = document.createElement('button');
+        pipedToggle = document.createElement('button');
+
+        setupCommonStyles(youtubeToggle, pipedToggle);
+
+        youtubeToggle.textContent = 'Перейти на Piped';
+        pipedToggle.textContent = 'Перейти на YouTube';
+
+        videoContainer.appendChild(youtubeToggle);
+        //videoContainer.appendChild(pipedToggle);
+
+        youtubeToggle.addEventListener('click', () => toggleRedirect({ preventDefault: () => {} }));
+        pipedToggle.addEventListener('click', () => toggleRedirect({ preventDefault: () => {} }));
+
+        addHoverEffect(youtubeToggle);
+        addHoverEffect(pipedToggle);
+    }
+
+    function positionPipedToggleButton() {
+        const videoContainer = document.querySelector('.flex.flex-wrap.gap-1');
+        if (!videoContainer) return;
+
+        pipedToggle = document.createElement('button');
+        youtubeToggle = document.createElement('button');
+
+        setupCommonStyles(youtubeToggle, pipedToggle);
+
+        pipedToggle.textContent = 'Перейти на YouTube';
+        youtubeToggle.textContent = 'Перейти на Piped';
+
+        //videoContainer.appendChild(youtubeToggle);
+        videoContainer.appendChild(pipedToggle);
+
+        youtubeToggle.addEventListener('click', () => toggleRedirect({ preventDefault: () => {} }));
+        pipedToggle.addEventListener('click', () => toggleRedirect({ preventDefault: () => {} }));
+
+        addHoverEffect(youtubeToggle);
+        addHoverEffect(pipedToggle);
+    }
+
+    function setupCommonStyles(button1, button2) {
+        button1.style.margin = '1vw';
+        button1.style.zIndex = '9999';
+        button1.style.padding = '10px 15px';
+        button1.style.backgroundColor = '#1e1e1e';
+        button1.style.color = '#ffffff';
+        button1.style.border = 'none';
+        button1.style.borderRadius = '5px';
+        button1.style.cursor = 'pointer';
+        button1.style.fontSize = '1.2em';
+        button1.style.transition = 'background-color 0.3s';
+
+        button2.style.margin = '1vw';
+        button2.style.zIndex = '9999';
+        button2.style.padding = '10px 15px';
+        button2.style.backgroundColor = '#1e1e1e';
+        button2.style.color = '#ffffff';
+        button2.style.border = 'none';
+        button2.style.borderRadius = '5px';
+        button2.style.cursor = 'pointer';
+        button2.style.fontSize = '1.2em';
+        button2.style.transition = 'background-color 0.3s';
+    }
+
+    function addHoverEffect(button) {
+        button.addEventListener('mouseover', () => {
+            button.style.backgroundColor = '#333333';
+        });
+        button.addEventListener('mouseout', () => {
+            button.style.backgroundColor = '#1e1e1e';
+        });
+    }
+
+    function updateToggleButtonText(youtubeToggle, pipedToggle, text) {
+        youtubeToggle.textContent = text;
+        pipedToggle.textContent = text;
+    }
+
+    window.addEventListener('load', manageToggles);
+    setInterval(manageToggles, 100);
+})();
